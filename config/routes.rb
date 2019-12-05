58a94f1eb8e70ef8c3ca9852e1104b3451f39e01
Majun1990 <merijun_a@hotmail.com>
2019-12-05 17:34:26 +0000

    corrected spelling, removed but on about us, added link to tracking button, created contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
   get 'resources', to: 'static_pages#resources'
   get 'faq', to: 'static_pages#faq'
+  get 'contact_us', to: 'static_pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
