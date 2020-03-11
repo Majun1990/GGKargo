a53c614450c8400a5f6eadbb15be0cea50889190
Majun1990 <merijun_a@hotmail.com>
2020-03-11 15:29:25 +0000

    re-did production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,26 @@
 Rails.application.configure do
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.sendgrid.net',
-  port:                 587,
-  domain:               'heroku.com',
-  user_name:            ENV['SENDGRID_USERNAME'],
-  password:             ENV['SENDGRID_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'ggkargo-majun-allen.herokuapp.com'}
+ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.sendgrid.net',
+#   port:                 587,
+#   domain:               'heroku.com',
+#   user_name:            ENV['SENDGRID_USERNAME'],
+#   password:             ENV['SENDGRID_PASSWORD'],
+#   authentication:       'plain',
+#   enable_starttls_auto: true }
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.default_url_options = { host: 'ggkargo-majun-allen.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
